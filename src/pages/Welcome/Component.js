581412054a87c061dc6dad318473af9aa998722b
Matt Davis <mdavis@virtustructure.com>
2020-07-03 08:24:27 -0400

    Updates for functionality

--- src/pages/Welcome/Component.js
+++ src/pages/Welcome/Component.js
@@ -20,7 +20,7 @@ import ReactPlayer from 'react-player'
 import fileDownload from 'js-file-download'
 import ReactStoreIndicator from 'react-score-indicator'
 import Timer from 'react-compound-timer'
-
+import Tour from 'reactour'
 import PoseNet from "react-posenet"
 
 import * as posenet from '@tensorflow-models/posenet';
@@ -60,8 +60,20 @@ async function drawSkeleton(ctx, keypoints) {
 
 function Welcome() {
 //export default class Welcome extends Component {
+  /* Flow States:
+    0 - Just arrived
+    1 - Person in View and Ready to Start
+    2 - Transitioning Animation to 2nd Image
+    3 - Start
+    4 - Playing
+    5 - Paused
+    6 - Finished
+  */
+  const [flowStage, setFlowstage] = React.useState(0);
   const [open, setOpen] = React.useState(true);
-
+  const [tourOpen, setTourOpen] = React.useState(false);
+  const [startTimer, setStartTimer] = React.useState(false)
+  const [resultOpen, setResultOpen] = React.useState(false);
   const [count, setCount] = useState(0)
   const [steppingState, setsteppingState] = useState(false)
   const [dancestep, setDancestep] = useState(0)
@@ -69,42 +81,74 @@ function Welcome() {
   const [points, setPoints] = useState(0)
   const [posesaver, setPosesaver] = useState(0)
   const canvasref = useRef(null)
+  const starttimerref = useRef(null)
   let stepping = false;
+
+
+
+
+
 
 
   const imitatearray = [
     "./images/Y.jpg",
     "./images/M.jpg",
-    "./images/C.jpg",
+    "./images/C2.jpg",
     "./images/A.jpg",
     "./images/Y.jpg",
     "./images/M.jpg",
-    "./images/C.jpg",
+    "./images/C2.jpg",
     "./images/A.jpg",
     "./images/Y.jpg",
     "./images/M.jpg",
-    "./images/C.jpg",
+    "./images/C2.jpg",
     "./images/A.jpg"
   ];
 
 // functions for state overlay
 
 
+
+// Initial Welcome Dialog
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
+
+    // TODO: Code to start recording of the canvas -
     var elements = document.getElementsByClassName("webcamerclass");
     let canvas = elements[0]
     let ctx = canvas.getContext("2d")
+    setStartTimer(true);
+
+    //starttimerref.startImmediately=false;
+    //setTourOpen(true)
     //canvas.fadeTo(3000,0)
     //ctx.globalAlpha = 0.2;
-    canvas.style.opacity = "0.4"
+    //canvas.style.opacity = "0.4"
     //canvas.style.display="none";
 
   };
+
+
+
+
+  // Tour Dialog handlers
+  const handleTourClose = () => {
+    setTourOpen(false)
+  }
+
+
+  const handleStartTimerClose = () => {
+    setStartTimer(false);
+  }
+
+  const handleResultClose = () => {
+    setResultOpen(false)
+  }
+
 
 
 
@@ -152,12 +196,9 @@ function Welcome() {
 
 
 
-// Reecording things
 
 
-
-
-  //Voice synth
+  //Voice synth Section
   var available_voices = window.speechSynthesis.getVoices();
 
 	// this will hold an english voice
@@ -183,8 +224,33 @@ function Welcome() {
 
 
 
-
+  // Function for posenet Estimate Response
   const onChange = (e) => {
+
+    // don't compute much if no pose data
+    if(!pose1[0] || !e[0]){
+      return
+    }
+
+    //First Flow Stage Check - Reset and Send to Next
+    if(flowStage == 0 && open && pose1[0] && e[0]) {
+      // if all points match then remove the warning and tell them to start the game
+      if(pose1[0].keypoints.length == e[0].keypoints.length){
+        handleClose()
+        utter.text = "Great. Now Match the picture."
+        window.speechSynthesis.speak(utter);
+      }
+
+    }
+
+    // Compute Accuracy if pose data not equal
+    if(pose1[0].keypoints.length > e[0].keypoints.length){
+      let smallpercentcalc = 0
+      let left = pose1[0].keypoints.length
+      let right = e[0].keypoints.length
+      smallpercentcalc = ((right / left ) * 50)
+      setCount(smallpercentcalc)
+    }
 
     if(e[0] && pose1[0]){
 
@@ -199,8 +265,11 @@ function Welcome() {
             setCount(posesimilarity1*100)
             if(posesimilarity1*100 > 95 && steppingState == false){
               let newdance1 = dancestep + 1;
-
-
+              let newpoints = hitPose(posesimilarity1, 2000)
+              if(newdance1 >= imagearray.length){
+                setResultOpen(true)
+                return;
+              }
               setsteppingState( (newdance2) => {
 
                 setDancestep((newdance3) => {
@@ -209,7 +278,7 @@ function Welcome() {
                   let ymca = ['Y', 'M', 'C', 'A', 'Y', 'M', 'C', 'A', 'Y', 'M', 'C', 'A']
                   utter.text = ymca[newdance1-1]
                   window.speechSynthesis.speak(utter);
-                  setPoints(points + 5);
+                  setPoints(points + newpoints);
                   setTimeout(function() { //Start the timer
 
                       setsteppingState(false)
@@ -250,13 +319,50 @@ function Welcome() {
     }
   };
   let state = { progress: 50 }
+
+  const steps = [
+  {
+    selector: '.points',
+    content: 'Points: How fast and accurate you are per pose.',
+  },
+  {
+    selector: '.timer',
+    content: 'Time: Time left in this game.',
+  },
+  {
+    selector: '.accuracy',
+    content: 'Accuracy: How accurate you are to matching the pose.',
+  },
+
+  ]
+
+  // Points functions on hitting the pose
+  const hitPose = (accuracy, timeleft) => {
+    let maxPoints = 1000;
+    let minPoints = 200;
+    // points 0 to 5 - will need a multiplier
+    let adjaccuracy = (accuracy*100 - 95) / 5;
+    //timeleft in ms
+    let timepoints =  (1 - ((9500 - timeleft) / 10000));
+    let scoreAllowed = (maxPoints - minPoints)
+    let randomness = Math.floor((Math.random() * 100) + 1);
+    let finalScore = scoreAllowed * ((timepoints * 0.6) + (adjaccuracy * 0.2) + ((randomness/100) * 0.2))
+    finalScore = Math.round(finalScore);
+    return finalScore
+  }
+
+
+
   return (
     <>
       <Meta
         title="Dance.Studio"
         description="Practice and Compete in fun dance challenges."
       />
-
+      <Tour
+        steps={steps}
+        isOpen={tourOpen}
+        onRequestClose={handleTourClose} />
 
 
         <PoseNet
@@ -267,7 +373,6 @@ function Welcome() {
         outputStride: 32,
         inputResolution: { width: 257, height: 200 },
         quantBytes: 2 }}
-          frameRate={2}
           input={step}
           onEstimate={poses => {
             pose1 = poses;
@@ -276,7 +381,6 @@ function Welcome() {
 
 
         <PoseNet
-        frameRate={5}
         style={{ position:"absolute", "zindex":10, left:"0", top:"0", width:"100%", height:"100%" }}
         className={"webcamerclass"}
         inferenceConfig={{ decodingMethod: "single-person", architecture: 'ResNet50',
@@ -288,27 +392,31 @@ function Welcome() {
         />
         <canvas ref={canvasref} height="500" width="600" className={"otherpose"} style={{ position:"absolute", "zindex":11, left:"0", top:"0", width:"100%", height:"100%" }}></canvas>
         <br/>
-        <div> <CircularProgress variant="static" value={count} style={{ position:"absolute", "zindex":20, left:"47.75%", bottom:"75px", width:"75px" }} color="secondary"/> </div>
+
+        <div > <CircularProgress variant="static" value={count} style={{ position:"absolute", "zindex":20, left:"47.75%", bottom:"75px", width:"75px" }} color="secondary"/> </div>
         <div> <CircularProgress variant="static" value={count} style={{ position:"absolute", "zindex":30, left:"46%", bottom:"75px", width:"100px" }} color="secondary"/> </div>
 
         <div>
         <Timer
-        initialTime={100 * 1000}
+        initialTime={60 * 1000}
         direction="backward"
         timeToUpdate={100}
         checkpoints={[
             {
                 time: 0,
-                callback: () => alert('countdown finished'),
+                callback: () => {
+                    setResultOpen(true)
+                    return;
+                },
             }
         ]}
     >  <div style={{ fontFamily: 'Helvetica Neue' }}>
 
-            <div style={{ fontSize: 48, position:"absolute", "zindex":10, left:"90%", top:"10%"}}>
+            <div className="points" style={{ fontSize: 48, position:"absolute", "zindex":10, left:"90%", top:"10%"}}>
                 { points }
             </div>
 
-            <div style={{ fontSize: 32, position:"absolute", "zindex":10, left:"50%", top:"85%" }}>
+            <div className="timer" style={{ fontSize: 32, position:"absolute", "zindex":10, left:"50%", top:"85%" }}>
 
                 <Timer.Seconds />
             </div>
@@ -317,8 +425,8 @@ function Welcome() {
         </div>
     </Timer></div>
 
-    <Snackbar open={steppingState} autoHideDuration={6000} >
-  <Alert open={steppingState} severity="success" autoHideDuration={6000}>
+    <Snackbar >
+  <Alert open={steppingState} severity="success">
     6000 Points!
   </Alert>
 </Snackbar>
@@ -345,25 +453,133 @@ function Welcome() {
       </Dialog>
     </div>
 
-    <div >
+    <div>
         <Dialog
-          open={open}
-          onClose={handleClose}
+          open={resultOpen}
+          onClose={handleResultClose}
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description">
           <DialogTitle id="alert-dialog-title">{"Great Job"}</DialogTitle>
           <DialogContent>
             <DialogContentText id="alert-dialog-description">
-              Final Results! Points 1 Million
+              Congrats! {points} Points!
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose} color="primary" autoFocus>
+            <Button onClick={handleResultClose} color="primary" autoFocus>
               Close
             </Button>
           </DialogActions>
         </Dialog>
       </div>
+
+
+      <div >
+          <Dialog
+            open={startTimer}
+            onClose={handleStartTimerClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description">
+            <DialogTitle id="alert-dialog-title">{"Starting in..."}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+              <Timer
+
+              initialTime={5 * 1000}
+              direction="backward"
+              timeToUpdate={200}
+              startImmediately={true}
+              ref={starttimerref}
+              checkpoints={[
+                  {
+                      time: 0,
+                      callback: () => {
+                        setStartTimer(false)
+                        utter.text = "Match the pose"
+                        window.speechSynthesis.speak(utter);
+
+                      }},
+                      {
+                      time: 4000,
+                      callback: () => {
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.9"
+
+                      }},
+                      {
+                      time: 3500,
+                      callback: () => {
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.8"
+
+                      }},
+                      {
+                      time: 3000,
+                      callback: () => {
+                        utter.text = "3"
+                        window.speechSynthesis.speak(utter);
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.7"
+
+                      }},
+                      {
+                      time: 2500,
+                      callback: () => {
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.6"
+
+                      }},
+                      {
+                      time: 2000,
+                      callback: () => {
+                        utter.text = "2"
+                        window.speechSynthesis.speak(utter);
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.5"
+
+                      }},
+                      {
+                      time: 1500,
+                      callback: () => {
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.4"
+
+                      }},
+                      {
+                      time: 1000,
+                      callback: () => {
+                        utter.text = "1"
+                        window.speechSynthesis.speak(utter);
+                        var elements = document.getElementsByClassName("webcamerclass");
+                        let canvas = elements[0]
+                        canvas.style.opacity = "0.3"
+
+                      }},
+
+              ]}
+          >
+
+              <div >
+                  <div className="timer" style={{ fontSize: 48, fontFamily: 'Helvetica Neue' }}>
+                      <Timer.Seconds />
+                  </div>
+              </div>
+          </Timer>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleStartTimerClose} color="primary" autoFocus>
+                Close
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
 
     </>
   );
